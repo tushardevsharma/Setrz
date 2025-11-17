335c9ffd448c146f2ba70b5441ae561d53e8da4d
tushardevsharma <devsharmatushar@gmail.com>
2025-11-17 16:05:40 +0530

    Add working loader

--- src/Setrz.Mobile/Setrz.Mobile/Features/Onboarding/StartingView.xaml.cs
+++ src/Setrz.Mobile/Setrz.Mobile/Features/Onboarding/StartingView.xaml.cs
@@ -2,7 +2,7 @@
 
 namespace Setrz.Mobile.Features.Onboarding;
 
-public partial class StartingView : BaseContentPage
+public partial class StartingView : ContentPage
 {
     public StartingView(StartingViewModel viewModel)
     {
