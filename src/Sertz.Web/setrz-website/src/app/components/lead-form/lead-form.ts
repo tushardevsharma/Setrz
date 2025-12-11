bc4bef96a41039f3de23f8c573d00f62e030d7fc
Tushar Sharma <tushar.sharma@mediassist.in>
2025-12-11 21:16:49 +0530

    Use hubspot's lead generation form

--- src/Sertz.Web/setrz-website/src/app/components/lead-form/lead-form.ts
+++ src/Sertz.Web/setrz-website/src/app/components/lead-form/lead-form.ts
@@ -1,33 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lead-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './lead-form.html',
   styleUrl: './lead-form.scss'
 })
 export class LeadForm {
-  private fb = inject(FormBuilder);
-  leadForm: FormGroup;
-
-  constructor() {
-    this.leadForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', Validators.required],
-      movingDate: [''],
-      message: ['']
-    });
-  }
-
-  onSubmit() {
-    if (this.leadForm.valid) {
-      console.log('Form Submitted!', this.leadForm.value);
-      // Here you would typically send the data to a server
-      alert('Thank you for your submission! We will be in touch shortly.');
-      this.leadForm.reset();
-    }
-  }
 }
